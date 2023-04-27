c648909f52351e950f5543b4fb1200935dd4cac9
Matt Hayes <matt@mysterycommand.com>
2023-04-26 20:05:04 -0500

    fix: missed some logic with hex digits

--- src/client/randomHex.ts
+++ src/client/randomHex.ts
@@ -1,5 +1,12 @@
-export const randomHex = (size = 3): string =>
+/**
+ * note: each hexadecimal digit is represented by a 2-character string, you can
+ * think of them like "color channels" if you're generating a color, the first
+ * digit is red, then green and blue
+ *
+ * @param length the number of hexadecimal digits to generate
+ * @returns a hexadecimal string of `length * 2`
+ */
+export const randomHex = (length: number = 3) =>
   crypto
-    .getRandomValues(Uint8Array.from({ length: size }))
-    .reduce((acc, value) => acc + value.toString(16), "")
-    .padStart(size * 2, "0");
+    .getRandomValues(Uint8Array.from({ length }))
+    .reduce((acc, value) => acc + value.toString(16).padStart(2, "0"), "");
